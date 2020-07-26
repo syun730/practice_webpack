1fe627861f174f59ddb1b2371c4a46c325dd1e25
syun <syun.divetolife@gmail.com>
2020-07-26 20:49:30 +0900

    babel-loader追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,34 @@
 const path = require('path');
+const { plugins } = require('./postcss.config');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: 'development',
   devtool: 'none',
   // entry: './src/app.js', // すべてのファイルの基準
-  entry: {app: './src/app.js'}, // 変数化すべてのファイルの基準
+  // entry: {app: './src/app.js'}, // 変数化すべてのファイルの基準
   // entry: ['./src/app.js', './src/sub.js'] // 複数ある場合は配列にする・依存関係が無い場合
-  // entry: {
-  //   app: './src/app.js',
-  //   sub: './src/sub.js'
-  // }, // ファイルを分けたい場合
+  entry: {
+    app: './src/app.js',
+    sub: './src/sub.js'
+  }, // ファイルを分けたい場合
   output: {
     path: path.resolve(__dirname, 'public'), // パスを変更したい場合
     // filename: 'bundle.js' // ファイル名を変更したい場合
-    filename: '[name].bundle.js' // 変数化ファイル名を変更したい場合
+    filename: '[name].js' // 変数化ファイル名を変更したい場合
   },
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
         test: /\.scss$/, // 正規表現 $は文末
         use: [
-          'style-loader',
+          // 'style-loader',
+          MiniCssExtractPlugin.loader,
           'css-loader',
           'postcss-loader',
           'sass-loader'
@@ -41,6 +49,12 @@ module.exports = {
         ]
       }
     ]
-  }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      // chunkFilename: '[name].[hash].css'
+    }),
+  ]
 }
 // CommonJs
